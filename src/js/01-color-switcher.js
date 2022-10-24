873102e85a90fa0ac4b87704185007dbf7a01f9f
Maksim6767 <nikmaksim20@gmail.com>
2022-10-25 00:08:36 +0300

    Did switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const body = document.body;
+const startSwitcherColor = document.querySelector('button[data-start]');
+const stopSwitcherColor = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onStartBtnClick = () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+   
+    startSwitcherColor.disabled = true;
+    stopSwitcherColor.disabled = false;
+};
+
+const onStopBtnClick = () => {
+    clearInterval(timerId);
+    
+    startSwitcherColor.disabled = false;
+    stopSwitcherColor.disabled = true;
+};
+
+startSwitcherColor.addEventListener ('click', onStartBtnClick);
+stopSwitcherColor.addEventListener('click', onStopBtnClick);
+
